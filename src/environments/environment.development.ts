cd96b16a1d75deb1cda21cfe1f3fe2f4a5cf0b86
SajidAhmed0 <mjsahmed205@gmail.com>
2024-03-28 22:03:38 +0530

    sfs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,2 +1,12 @@
 export const environment = {
+    firebaseConfig : {
+        apiKey: "AIzaSyCzJamhmXcQrDlveboD4VnGo0s9oaRvSw0",
+        authDomain: "my-application-fac93.firebaseapp.com",
+        databaseURL: "https://my-application-fac93-default-rtdb.firebaseio.com",
+        projectId: "my-application-fac93",
+        storageBucket: "my-application-fac93.appspot.com",
+        messagingSenderId: "544287995510",
+        appId: "1:544287995510:web:781f8af811fce442929157",
+        measurementId: "G-VJYBP0CNSL"
+    }
 };
