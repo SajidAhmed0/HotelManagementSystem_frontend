067e50f7693204ab41387af0520a2e5a59fc3f08
SajidAhmed0 <mjsahmed205@gmail.com>
2024-04-05 20:28:24 +0530

    more updattion

--- src/app/add-roomtype/add-roomtype.component.ts
+++ src/app/add-roomtype/add-roomtype.component.ts
@@ -13,7 +13,7 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './add-roomtype.component.html',
   styleUrl: './add-roomtype.component.scss'
 })
-export class AddRoomtypeComponent {
+export class AddRoomtypeComponent implement OnInit {
 
   facilities: Array<{
     name: string,
@@ -44,5 +44,52 @@ export class AddRoomtypeComponent {
 
   removeFacility(i: any){
     this.facilities.splice(i, 1);
+  }
+
+  roomtype$!: Observable<any>;
+  hotelId: any;
+  roomtypeId: any;
+
+  roomTypeName: any;
+  roomTypeDescription: any;
+  roomTypeMaxAdult: any;
+
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private hotelService: HotelServiceService,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.roomtype$ = this.activatedRoute.paramMap.pipe(
+      switchMap((paramsMap) => {
+        this.roomtypeId = paramsMap.get('roomtypeId');
+        this.hotelId = paramsMap.get('hotelId');
+
+        let roomtype = this.hotelService.getRoomType(this.roomtypeId);
+        
+        return roomtype;
+      })
+    );
+    this.roomtype$.subscribe(r => {
+      this.roomTypeName = r.name;
+      this.roomTypeDescription = r.description;
+      this.roomTypeMaxAdult = r.maxAdult;
+    });
+  }
+
+  cancel(){
+    this.router.navigate([`adminOneHotel/${this.hotelId}`]);
+  }
+
+  async editRoomtype(){
+    const roomtype = {
+      name: this.roomTypeName,
+      description: this.roomTypeDescription,
+      maxAdult: this.roomTypeMaxAdult
+    };
+
+    let r = await this.hotelService.updateRoomType(this.roomtypeId, roomtype).toPromise();
+    this.router.navigate([`adminOneHotel/${this.hotelId}`]);
   }
 }
