4a71e9cddc69029f8ba4c5516e3c57487c73a811
SajidAhmed0 <mjsahmed205@gmail.com>
2024-04-06 22:14:28 +0530

    initial setup for admin side

--- src/app/admin-hotels/admin-hotels.component.ts
+++ src/app/admin-hotels/admin-hotels.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+import { FooterComponent } from "../footer/footer.component";
+import { AsyncPipe, NgIf } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-admin-hotels',
-  standalone: true,
-  imports: [],
-  templateUrl: './admin-hotels.component.html',
-  styleUrl: './admin-hotels.component.scss'
+    selector: 'app-admin-hotels',
+    standalone: true,
+    templateUrl: './admin-hotels.component.html',
+    styleUrl: './admin-hotels.component.scss',
+    imports: [
+      NavbarComponent,
+      FooterComponent,
+      NgIf,
+      AsyncPipe
+    ]
 })
-export class AdminHotelsComponent {
+export class AdminHotelsComponent implements OnInit {
+  hotels$!: Observable<any>;
 
+
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
